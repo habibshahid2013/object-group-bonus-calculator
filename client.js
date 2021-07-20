595501ca1d330ec8da6f30aa2466308c42667cd8
Hassan Shahid <habibshahid2013@gmail.com>
2021-07-20 16:19:52 -0500

    Final changes before Css

--- client.js
+++ client.js
@@ -29,43 +29,7 @@ const employees = [
     annualSalary: '35000',
     reviewRating: 1
   }
-];function employeeBonuses(object){
-  let bonusResults = bonusCalulation(object)
-  return {name: object.name, 
-  bonusPecentages: bonusResults,
-  totalCompensation: (bonusResults * object.annualSalary) + object.anualSalary,
-  totalBonus: bonusResults * object.annualSalary}
-}
-function bonusCalulation(object){
-  let bonus = 0
-  if (object.reviewRating <= 2){
-    return bonus;
-  }
-  else if ( object.reviewRating === 3){
-    bonus += .04;
-  }
-  else if (object.reviewRating === 4){
-    bonus += .06;
-  }
-  else if ( object.reviewRating === 5){
-    bonus += .10;
-  }
-  if ( object.employeeNumber.length === 4){
-    bonus += .05;}
-  if (object.annualSalary > 65000) {
-    bonus -= .01 ;
-  }
-  //if bonus is greater than or equal  13 percent than bonus equal 
-  if ( bonus > .13 ){
-    bonus = .13
-  }
-  if (bonus < 0){
-    bonus = 0
-  }
-  return bonus;
-}
-
-
+];
 
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
 
@@ -76,4 +40,67 @@ function bonusCalulation(object){
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
+//processed employee bonuses by looping through employee objects, 
+//and logged employee bonus function.
+function processEmployeeBonuses(Employees){
+  let string = "" ; 
+  let salaryDom = $("#salaries") ; 
+  salaryDom.empty() ;
+  for (const employee of employees) {
+    employeeToPrint = employeeBonuses(employee); 
+    string += `<li> Name: ${employeeToPrint.name} Bonus Percentages: ${employeeToPrint.bonusPecentages} Total Compensation: ${employeeToPrint.totalCompensation} </li>` ;
+  }
+  salaryDom.append(string);
+}
+//Created new object. 
+function employeeBonuses(object){
+  let bonusResults = bonusCalulation(object)
+  return {name: object.name, 
+  bonusPecentages: bonusResults,
+  totalCompensation: (Math.round(bonusResults * Number(object.annualSalary))) + Number(object.annualSalary),
+  totalBonus: Math.round(bonusResults * Number(object.annualSalary))}
+}
+function bonusCalulation(object){
+  let bonus = 0
+  
+  if (object.reviewRating <= 2){
+    return bonus;
+  } else if ( object.reviewRating === 3){
+    bonus += .04;
+  } else if (object.reviewRating === 4){
+    bonus += .06;
+  } else if ( object.reviewRating === 5){
+    bonus += .10;
+  }
+
+  if ( object.employeeNumber.length === 4){
+    bonus += .05;
+  }
+
+  if (Number(object.annualSalary) > 65000) {
+    bonus -= .01 ;
+  }
+
+  //if bonus is greater than or equal  13 percent than bonus equal 
+  if ( bonus > .13 ){
+    bonus = .13
+  }
+
+  if (bonus < 0){
+    bonus = 0
+  }
+  return bonus;
+}
+
 console.log( employees );
+
+//Used for testing before adding JQuery
+//processEmployeeBonuses();
+
+console.log('JS');
+
+$(document).ready(readyNow);
+function readyNow() {
+  console.log('JQ');
+  $('#accessButton').on('click', processEmployeeBonuses );
+}
